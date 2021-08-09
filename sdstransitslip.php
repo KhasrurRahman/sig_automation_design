77c655d05665c40c91e0846903dea147a7f48fa4
labony277 <45601560+labony277@users.noreply.github.com>
2021-08-09 00:12:20 -0700

    update v5(8-9-21)

--- sdstransitslip.php
+++ sdstransitslip.php
@@ -22,7 +22,7 @@
                                                 <br>
                                                 
                                                 <svg width="150" height="100">
-                                            <rect width="150" height="100" style="fill:rgb(255,255,255);stroke-width:4;stroke:rgb(23, 23, 26)" />
+                                            <rect width="150" height="100" style="fill:rgb(255,255,255);stroke-width:4;stroke:#006500" />
      
                                                  </svg>
                                             
@@ -34,16 +34,35 @@
                                                 <br>
                                                 
                                                 <svg width="150" height="100">
-                                            <rect width="150" height="100" style="fill:rgb(255,255,255);stroke-width:4;stroke:rgb(23, 23, 26); " />
+                                            <rect width="150" height="100" style="fill:rgb(255,255,255);stroke-width:4;stroke:#006500; " />
      
                                                  </svg>
                                         </div>
                                     </div>
+                                    <br>
 
-                                    <div class="row" style="text-align: center;">
+                                    <div class="row" >
                                         <div class="col-12">
-                                            <p style="font-size: 14px;">Transit Slip/Bag no.<h1></h1></p>
+                                            <p style="font-size: 18px;margin-left: 148px;margin-bottom: 2px;color: #006500">Transit Slip/Bag no<h1 style="width: 52%;    margin-left: 332px;  border-bottom: 3px solid #006500;"></h1></p>
                                         </div>
+                                        <br>
+                                        <div class="col-12">
+                                            <p style="font-size: 18px;margin-left: 148px;margin-bottom: 2px;color: #006500">From<h1 style="width: 52%;    margin-left: 332px;  border-bottom: 3px solid #006500;"></h1></p>
+                                        </div>
+                                        <br>
+                                        <div class="col-12">
+                                            <p style="font-size: 18px;margin-left: 148px;margin-bottom: 2px;color: #006500">To<h1 style="width: 52%;    margin-left: 332px;  border-bottom: 3px solid #006500;"></h1></p>
+                                        </div>
+                                        <br>
+                                        <div class="col-12">
+                                            <p style="font-size: 18px;margin-left: 148px;margin-bottom: 2px;color: #006500">Route or Method<h1 style="width: 52%;    margin-left: 332px;  border-bottom: 3px solid #006500;"></h1></p>
+                                        </div>
+                                        <br>
+                                        <div class="col-12">
+                                            <p style="font-size: 18px;margin-left: 148px;margin-bottom: 2px;color: #006500">Name of Courier/DR<h1 style="width: 52%;    margin-left: 332px;  border-bottom: 3px solid #006500;"></h1></p>
+                                        </div>
+                                      
+
                                     </div>
                                    
                                 </div>
@@ -137,6 +156,26 @@
                                     </div>
 
                                    
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h3>No. of UR/R packages</h3>
+                                        <div class="col-md-6">
+                                           
+                                        <h5 style="font-size: 18px;margin-bottom: 2px;color: #006500">Despatched By<h1 style="width: 380%; margin-left: 38px;   border-bottom: 3px solid #006500;"></h1></h5>
+                                        <br>
+                                        <br>
+                                        <h5 style="font-size: 18px;margin-bottom: 2px;color: #006500">Time<h1 style="width: 300%; margin-left: 45px;   border-bottom: 3px solid #006500;"></h1></h5>
+                                        <br>  
+                                        <h5 style="font-size: 18px;margin-bottom: 2px;color: #006500">Date<h1 style="width: 300%; margin-left: 45px;   border-bottom: 3px solid #006500;"></h1></h5>
+                                        
+                                        </div>
+                                        <div class="col-md-6"style="text-align: right;">
+                                            
+
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
